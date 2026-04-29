65b1f18ef5c5d2a33917250af8ffa696b17f08b5
archan-sureja <archan.sureja@simformsolutions.com>
2026-04-29 15:17:22 +0530

    fixed import

--- backend/assignments/views.py
+++ backend/assignments/views.py
@@ -3,7 +3,8 @@ from rest_framework.viewsets import ModelViewSet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.exceptions import PermissionDenied
 from rest_framework.decorators import action 
-from rest_framework.response import Response , 
+from rest_framework.response import Response
+from django.http import FileResponse
 from rest_framework import generics 
 from rest_framework import status
 from rest_framework.views import APIView
